852207aa50b75b6846fcf6ee6c040a3524e7540c
Christoph Steup <steup@ivs.cs.ovgu.de>
2014-04-23 02:29:02 +0200

    Added File Logger
      - Log class for logging into file
      - Prototypical integration into remote application
      - extension of config file

---
+++ include/Log.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <ctime>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+
+class Log : public boost::filesystem::ofstream{
+  private:
+    boost::filesystem::path mFileName;
+
+    std::ostream& date(){
+      char buffer[128];
+      std::time_t now=std::time(nullptr);
+      std::strftime(buffer, sizeof(buffer), "%F %T", std::localtime(&now));
+      static_cast<std::ostream&>(*this) << "[" << buffer << "] ";
+      return *this;
+    }
+
+  public:
+    Log(const Log&) = delete;
+    Log& operator=(const Log&) = delete;
+
+    Log(const boost::filesystem::path& fileName) : 
+      boost::filesystem::ofstream(fileName, std::ios_base::app),
+      mFileName(fileName)
+    {
+      date() << "Opening Log: " << mFileName.native() << std::endl;
+    }
+
+    virtual ~Log(){
+      *this << "Closing Log: " << mFileName.native() << std::endl;
+    }
+
+    template<typename T>
+    std::ostream& operator<<(T value){
+      date();
+      return static_cast<std::ostream&>(*this) << value;
+    }
+    std::ostream& operator<<(std::ostream& (*func)(std::ostream&)){
+      return static_cast<std::ostream&>(*this) << func;
+    }
+};
--- src/remote.cpp
+++ src/remote.cpp
@@ -1,5 +1,6 @@
 #include <CommSender.h>
 #include <Joystick.h>
+#include <Log.h>
 
 #include <chrono>
 #include <iostream>
@@ -22,63 +23,75 @@ using namespace boost::filesystem;
 
 int main(int argc, char** argv){
   try{
-  string progName=path(argv[0]).filename().native();
-  path defaultConfigFile = current_path();
-  defaultConfigFile/="cfg";
-  defaultConfigFile/=progName+".cfg";
+    string progName=path(argv[0]).filename().native();
+    path defaultConfigFile = current_path();
+    defaultConfigFile/="cfg";
+    defaultConfigFile/=progName+".cfg";
 
-  options_description options(progName);
-  options.add_options()
-    ("configFile", value<path>()->default_value(defaultConfigFile), "Path to configuration file")
-    ("Connection.host", value<string>(), "Name of host to control")
-    ("Connection.port", value<uint16_t>(), "Port of host to control")
-    ("Connection.timeout", value<unsigned long>(), "Timeout of periodic transmission in milliseconds")
-    ("Joystick.id", value<unsigned int>(), "Id of joystick to use")
-    ("listJoysticks", "List available joysticks by ID")
-    ("help", "print this help");
+    options_description options(progName);
+    options.add_options()
+      ("configFile", value<path>()->default_value(defaultConfigFile), "Path to configuration file")
+      ("Log.file", value<string>(), "File to log errors or - for stdout")
+      ("Connection.host", value<string>(), "Name of host to control")
+      ("Connection.port", value<uint16_t>(), "Port of host to control")
+      ("Connection.timeout", value<unsigned long>(), "Timeout of periodic transmission in milliseconds")
+      ("Joystick.id", value<unsigned int>(), "Id of joystick to use")
+      ("listJoysticks", "List available joysticks by ID")
+      ("help", "print this help");
 
-  variables_map vm;
-  store(parse_command_line(argc, argv, options), vm);
-  if(vm.count("listJoysticks")){
-    cout << "Available Joysticks: " << endl;
-    for(unsigned int i=0;i<Joystick::getNumJoysticks();i++)
-      cout << "\t" << i << Joystick::getAvailableJoysticks().at(i) << endl;
-    return 0;
+    variables_map vm;
+    store(parse_command_line(argc, argv, options), vm);
+    
+    if(vm.count("listJoysticks")){
+      cout << "Available Joysticks: " << endl;
+      for(unsigned int i=0;i<Joystick::getNumJoysticks();i++)
+        cout << "\t" << i << " " << Joystick::getAvailableJoysticks().at(i) << endl;
+      return 0;
+    }
+    if(vm.count("help")){
+      cerr << options << endl;
+      return -1;
+    }
+
+    cout << "Mobile PI Remote Control Application:" << endl;
+    ifstream configFile(vm["configFile"].as<path>());
+    cout << "\tConfig file: " << vm["configFile"].as<path>() << endl;
+    store(parse_config_file(configFile, options), vm);
+
+    Log errorLog(vm["Log.file"].as<string>());
+
+    Joystick   joystick  (vm["Joystick.id"]       .as<unsigned int>());
+    CommSender connection(vm["Connection.host"]   .as<string>(),
+                          vm["Connection.port"]   .as<uint16_t>(),
+                          milliseconds(vm["Connection.timeout"].as<unsigned long>()));
+    const auto& parameters=connection.getParameters();
+
+    cout << "\t" << connection << endl;
+    cout << "\t" << joystick   << endl;
+    cout << "\t" << parameters << endl;
+
+    auto joystickHandler = [&](const Joystick::State& state){
+      cout << state.axes[3] << ", " << state.axes[0] << endl;
+      int16_t speed=(int32_t)state.axes[3]*parameters.maxSpeed/state.axisMax;
+      int16_t angle=(int32_t)state.axes[0]*parameters.maxAngle/state.axisMax;
+      cout << speed << ", " << angle << endl;
+      connection.setMoveData({speed, angle});
+    };
+
+    auto errorHandler = [&](std::exception& e){
+      errorLog << e.what() << std::endl;
+    };
+
+    joystick.addEventHandler(joystickHandler);
+    joystick.addErrorHandler(errorHandler);
+    connection.addErrorHandler(errorHandler);
+
+    pause();
   }
-  if(vm.count("help")){
-    cerr << options << endl;
-    return -1;
-  }
-
-  cout << "Mobile PI Remote Control Application:" << endl;
-  ifstream configFile(vm["configFile"].as<path>());
-  cout << "\tConfig file: " << vm["configFile"].as<path>() << endl;
-  store(parse_config_file(configFile, options), vm);
-
-  Joystick   joystick  (vm["Joystick.id"]       .as<unsigned int>());
-  CommSender connection(vm["Connection.host"]   .as<string>(),
-                        vm["Connection.port"]   .as<uint16_t>(),
-                        milliseconds(vm["Connection.timeout"].as<unsigned long>()));
-  const auto& parameters=connection.getParameters();
-
-  cout << "\t" << connection << endl;
-  cout << "\t" << joystick   << endl;
-  cout << "\t" << parameters << endl;
-
-  auto joystickHandler = [&](const Joystick::State& state){
-    cout << state.axes[3] << ", " << state.axes[0] << endl;
-    int16_t speed=(int32_t)state.axes[3]*parameters.maxSpeed/state.axisMax;
-    int16_t angle=(int32_t)state.axes[0]*parameters.maxAngle/state.axisMax;
-    cout << speed << ", " << angle << endl;
-    connection.setMoveData({speed, angle});
-  };
-
-  joystick.addEventHandler(joystickHandler);
-
-  pause();
-  }
-  catch(boost::exception& e){
-    cerr << boost::diagnostic_information(e);
+  catch(std::exception& e){
+    cerr << std::endl;
+    cerr << "Error during startup of program:" << std::endl;
+    cerr << "\t" << e.what() << std::endl;
   }
 
   return 0;
