ffe88b68a6f819de2774c05d7b6c1b10e51ff973
Christoph Steup <steup@ivs.cs.ovgu.de>
2014-07-17 00:21:52 +0200

    Removed SDL from test target

--- make/test.mk
+++ make/test.mk
@@ -7,9 +7,6 @@ include make/pkg-config.mk
 PKG_NAME      := gstreamer-video-1.0
 include make/pkg-config.mk
 
-PKG_NAME      := sdl
-include make/pkg-config.mk
-
 CXXFLAGS      += -march=native
 
 OBJECTS  := test VideoStream VideoGUI
