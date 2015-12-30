45da8c8e80a1f985f7377c611e137118d2cb8826
Christoph Steup <steup@ivs.cs.ovgu.de>
2015-12-30 23:06:50 +0000

    Make: Add compiler specification for distcc support

--- make/rpi2.mk
+++ make/rpi2.mk
@@ -1,6 +1,8 @@
-CXXFLAGS     += -march=armv7a 
-LIBS         += bcm2835
+CC        := armv7a-hardfloat-linux-gnueabi-gcc
+CXX       := armv7a-hardfloat-linux-gnueabi-g++
+CXXFLAGS  += -march=armv7-a 
+LIBS      += bcm2835
 
-OBJECTS := ctrl Motor Servo CommReceiver CommBase
+OBJECTS   := ctrl Motor Servo CommReceiver CommBase
 
-TARGET       := ctrl
+TARGET    := ctrl
