418060e7b9c4170eeedd84cae3c2a2ffff85db91
Christoph Steup <steup@ivs.cs.ovgu.de>
2016-01-11 01:26:03 +0100

    Avr-Ctrl: Support for Motor, Servo and SpiComm
            - Servo now working
            - Servo abstraction taking FixPoint angle
            - Motor abstraction taking FixPoint speed
            - Preliminary SpiComm based on seperate SpiSlave and Protocol
              driver
            - TODO: Implement Write support
            - TODO: Implement Protocol

--- src/avr/avrCtrl/src/ctrl.cpp
+++ src/avr/avrCtrl/src/ctrl.cpp
@@ -1,31 +1,26 @@
 #include <platform.h>
 
 #include <avr-halib/drivers/avr/pwm.h>
+#include <avr-halib/drivers/ext/led.h>
 #include <avr-halib/interrupts/interrupt.h>
+#include <avr-halib/interrupts/InterruptManager/SignalSemanticInterrupt.h>
+#include <avr-halib/common/fixPoint.h>
+#include <avr-halib/drivers/avr/externalInterrupt.h>
+#include <avr-halib/common/delegate.h>
 
-typedef avr_halib::interrupts::interrupt_manager::InterruptManager<> IM;
+#include <boost/mpl/vector.hpp>
+
+typedef avr_halib::object::FixPointValue<int32_t, 4> Angle;
+typedef Angle Speed;
+
 
 using avr_halib::regmaps::local::Timer1;
 using avr_halib::regmaps::local::Timer2;
 using avr_halib::config::PWMDefaultConfig;
 using avr_halib::drivers::avr::PWMGenerator;
+using avr_halib::drivers::ext::Led;
 
-struct PWM1Config : public PWMDefaultConfig< Timer1 >
-{
-    enum ChannelUsage
-    {
-        useChannelA = true,
-        useChannelB = true,
-        useChannelC = true
-    };
-
-    static const pwm::Cycle      cycle      = pwm::static10;
-    static const pwm::OutputMode outputMode = pwm::normal;
-    static const pwm::Type       correction = pwm::phaseCorrect;
-    static const pwm::Prescalers ps         = pwm::ps64;
-};
-
-struct PWM2Config : public PWMDefaultConfig< Timer2 >
+struct MotorConfig : public PWMDefaultConfig< Timer2 >
 {
     enum ChannelUsage
     {
@@ -36,39 +31,199 @@ struct PWM2Config : public PWMDefaultConfig< Timer2 >
 
     static const pwm::Cycle      cycle      = pwm::static8;
     static const pwm::OutputMode outputMode = pwm::normal;
-    static const pwm::Type       correction = pwm::phaseCorrect;
+    static const pwm::Type       correction = pwm::phaseFreqCorrect;
     static const pwm::Prescalers ps         = pwm::ps256;
 };
 
-typedef PWMGenerator< PWM2Config > PWM2;
-typedef PWMGenerator< PWM1Config > PWM1;
+struct ServoConfig : public PWMDefaultConfig< Timer1 >
+{
+    enum ChannelUsage
+    {
+        useChannelA = true,
+				useChannelB = false,
+				useChannelC = false
+    };
+
+    static const pwm::Cycle      cycle      = pwm::static10;
+    static const pwm::OutputMode outputMode = pwm::normal;
+    static const pwm::Type       correction = pwm::fast;
+    static const pwm::Prescalers ps         = pwm::ps256;
+};
+
+typedef PWMGenerator< ServoConfig > Servo;
+typedef PWMGenerator< MotorConfig > Motor;
+
+Servo servo;
+Motor motor;
+
+/** \brief change steering angle of car
+ *  \param angle steering angle in milliDeg (left is negative)
+ */
+void steer(Angle angle) {
+  const Angle ticksPerUS(1000000ULL*Servo::TickFrequency::denominator/Servo::TickFrequency::numerator);
+  const Angle maxAngle(30);
+  const Angle maxUS(2000);
+  const Angle straightUS(1500);
+  const Angle calibFactor(-1);
+  const Angle calibOffset(0);
+  angle = angle * calibFactor + calibOffset;
+  angle = angle * (maxUS-straightUS) / maxAngle;
+  angle = (angle + straightUS) / ticksPerUS;
+  log::emit() << "Steering angle: " << angle << log::endl;
+  servo.value<Servo::channelA>(angle.trunc());
+}
+
+/** \brief change steering angle of car
+ *  \param angle steering angle in milliDeg (left is negative)
+ */
+void speed(Speed speed) {
+  const Speed maxSpeed(14);
+  UsePortmap(dir, platform::Motor);
+    dir.dir.port = speed>0;
+  SyncPortmap(dir);
+  speed = speed.abs() * Motor::pwmMax / maxSpeed;
+  log::emit() << "Speed: " << speed << log::endl;
+  motor.value<Motor::channelA>(speed.trunc());
+}
+
+using avr_halib::regmaps::local::Spi;
+using avr_halib::common::Delegate;
+
+
+class Protocol {
+  enum State {
+    idle,
+    write,
+    read
+  } mState;
+  enum Return {
+    ok,
+    invalidReg,
+    invalidOp,
+    invalidParam
+  } mReturn;
+  uint8_t mRxBuffer[129];
+  uint8_t mTxBuffer[129];
+  uint8_t mRxPtr;
+  uint8_t mTxPtr;
+
+  Delegate<uint8_t> transmitFunc;
+
+  public:
+    Protocol() : mState(idle), mReturn(ok), mRxPtr(0), mTxPtr(0) {}
+
+    void startFrame() {
+      mState  = idle;
+      mRxPtr  =    0;
+      mTxPtr  =    0;
+      mReturn =   ok;
+    }
+
+    void endFrame() {
+      mRxBuffer[mRxPtr] = '\0';
+      log::emit() << "Got SPI Packet: " << (char*)mRxBuffer << log::endl;
+    }
+    
+    void handleRxByte(uint8_t byte) {
+      mRxBuffer[mRxPtr++]=byte;
+      mRxPtr%=(sizeof(mRxBuffer)-1);
+    }
+};
+
+
+using namespace avr_halib::interrupts::interrupt_manager;
+using boost::mpl::vector;
+
+using avr_halib::drivers::avr::ExternalInterrupt;
+
+template<typename Proto, typename SpiRegMap, typename ExtIntRegMap> 
+class SpiSlave : public Proto,
+                 private ExternalInterrupt<ExtIntRegMap, ExtIntRegMap::both> {
+  private:
+    typedef Proto Protocol;
+    typedef ExternalInterrupt<ExtIntRegMap, ExtIntRegMap::both> FrameInt;
+  public:
+
+    static SpiSlave* sInstance;
+    
+    SpiSlave() : Protocol() {
+      
+      UseRegMap(rm, Spi);
+      rm.spie = true;
+      rm.spe = true;
+      rm.dord = false;
+      rm.mstr = false;
+      rm.cpol = false;
+      rm.cpha = false;
+      SyncRegMap(rm);
+
+      this->setPullUp(true);
+      this->FrameInt::enable();
+      sInstance = this;
+    }
+
+    void handleOp() {
+      UseRegMap(rm, Spi);
+      this->handleRxByte(rm.spdr);
+    }
+
+    void handleFrame() {
+      UseRegMap(rm, Spi);
+      SyncRegMap(rm);
+      if(rm.ss.pin)
+        this->endFrame();
+      else
+        this->startFrame();
+}
+  private:
+    typedef Slot<Spi::InterruptMap::operationComplete, Binding::SingletonFunction> SpiSlot;
+    typedef Slot<FrameInt::InterruptMap::externalInterrupt, Binding::SingletonFunction> SpiFrameSlot;
+    typedef typename SpiSlot::template Bind<SpiSlave, &SpiSlave::handleOp, &sInstance> BoundSpiSlot;
+    typedef typename SpiFrameSlot::template Bind<SpiSlave, &SpiSlave::handleFrame, &sInstance> BoundSpiFrameSlot;
+  public:
+    typedef typename vector<BoundSpiSlot, BoundSpiFrameSlot>::type InterruptSlotList;
+};
+
+template<typename P, typename I, typename R>
+SpiSlave<P, I, R>* SpiSlave<P, I, R>::sInstance = NULL;
+
+typedef avr_halib::regmaps::local::ExternalInterrupt6 SpiFrameInt;
+
+typedef SpiSlave<Protocol, Spi, SpiFrameInt> Comm;
+
+typedef InterruptManager< Comm::InterruptSlotList > IM;
 
 BIND_INTERRUPTS(IM);
 
 int main()
 {
-    PWM2 pwm2;
-    PWM1 pwm1;
 
-    log::emit() << "PWM2 Tick-Freq: " << PWM2::TickFrequency::numerator << " / "
-        << PWM2::TickFrequency::denominator << log::endl;
+    log::emit() << "Servo Tick-Size: " << (1000000ULL * Servo::TickFrequency::denominator / Servo::TickFrequency::numerator) << "  us" << log::endl;
+    log::emit() << "Servo PWM-Freq : " << (Servo::DutyFrequency::numerator / Servo::DutyFrequency::denominator) << "  Hz" << log::endl;
+    log::emit() << "Motor Tick-Size: " << (1000000ULL * Motor::TickFrequency::denominator / Motor::TickFrequency::numerator) << "  us" << log::endl;
+    log::emit() << "Motor PWM-Freq : " << (Motor::DutyFrequency::numerator / Motor::DutyFrequency::denominator) << "  Hz" << log::endl;
 
-    log::emit() << "PWM2 PWM-Freq: " << PWM2::DutyFrequency::numerator << " / "
-        << PWM2::DutyFrequency::denominator << log::endl;
-    
-		log::emit() << "PWM1 Tick-Freq: " << PWM1::TickFrequency::numerator << " / "
-        << PWM1::TickFrequency::denominator << log::endl;
+    Comm comm;
 
-    log::emit() << "PWM1 PWM-Freq: " << PWM1::DutyFrequency::numerator << " / "
-        << PWM1::DutyFrequency::denominator << log::endl;
+    motor.start();
+		servo.start();
 
-    pwm1.start();
-    pwm1.value<PWM1::channelA>(512);
-    pwm1.value<PWM1::channelB>(256);
-    pwm1.value<PWM1::channelC>(1000);
-    
-		pwm2.start();
-    pwm2.value<PWM2::channelA>(128);
+    UsePortmap(motorPins, platform::Motor);
+    motorPins.dir.ddr  = true;
+    motorPins.dir.port = platform::Motor::onLevel;
+    motorPins.reset.ddr = true;
+    motorPins.reset.port = true;
+    motorPins.ff1.port = true;
+    motorPins.ff2.port = true;
+    SyncPortmap();
+
+    Led<platform::StatusLed> status;
+    status.setOn();
+
+    steer(-10.0f);
+    speed(1.1f);
+
+    sei();
 
     while(true)
         Morpheus::sleep(Morpheus::SleepModes::idle);
