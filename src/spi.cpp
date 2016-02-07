d8b040e23b7a72dc95447cb805e2861ca7d59896
Christoph Steup <steup@ivs.cs.ovgu.de>
2016-02-07 02:21:22 +0100

    Switch to bcm2835 for SPI

--- src/spi.cpp
+++ src/spi.cpp
@@ -73,89 +73,52 @@ class SpiDev {
     void transfer(Message& msg);
 };
     
-void SpiDev::attr(unsigned long req, void* ptr, const char* attrName, bool set){
-  if(ioctl(mDev, req, ptr)==-1) {
-    std::ostringstream os;
-    os << "Error " << (set?"s":"g") << "etting SPI attribute " << attrName << " of device " << mDevName;
-    throw std::system_error(errno, std::system_category(), os.str());
-  }
-}
-
 SpiDev::SpiDev(const std::string device)
-  : mDevName(device), mDev(open(device.c_str(), O_RDWR)) {
-  if(mDev==-1) {
-    std::ostringstream os;
-    os << "Error opening SPI device " << mDevName;
-    throw std::system_error(errno, std::system_category(), os.str());
-  }
+  bcm2835_spi_begin();
 }
 
 SpiDev::~SpiDev(){
-  if(::close(mDev)==-1) {
-    std::ostringstream os;
-    os << "Error closing SPI device " << mDevName;
-    throw std::system_error(errno, std::system_category(), os.str());
-  }   
+  bcm2835_spi_end():
 }
 
 void SpiDev::set(Mode mode){
-  attr(SPI_IOC_WR_MODE, &mode.mode, mode.name(), true);
+  bcm2835_spi_setDataMode(mode.mode);
 }
 
 void SpiDev::get(Mode& mode){
-  attr(SPI_IOC_RD_MODE, &mode.mode, mode.name(), false);
 }
 
 void SpiDev::set(Speed speed){
-  attr(SPI_IOC_WR_MAX_SPEED_HZ, &speed.speed, speed.name(), true);
+  bcm2835_spi_setClockDivider(BCMD2835_SPI_CLOCK_DIVIDER_512);
 }
 
 void SpiDev::get(Speed& speed){
-  attr(SPI_IOC_RD_MAX_SPEED_HZ, &speed.speed, speed.name(), false);
 }
 
 void SpiDev::set(BitsPerWord bits){
-  attr(SPI_IOC_WR_BITS_PER_WORD, &bits.bits, bits.name(), true);
 }
 
 void SpiDev::get(BitsPerWord& bits){
-  attr(SPI_IOC_RD_BITS_PER_WORD, &bits.bits, bits.name(), false);
 }
 
 void SpiDev::set(BitAlignment align){
-  attr(SPI_IOC_WR_LSB_FIRST, &align.align, align.name(), true);
 }
 
 void SpiDev::get(BitAlignment& align){
-  attr(SPI_IOC_RD_LSB_FIRST, &align.align, align.name(), false);
 }
 
 void SpiDev::read(Message& msg){
- if(::read(mDev, msg.data(), msg.size())!=(ssize_t)msg.size()) {
-    std::ostringstream os;
-    os << "Error reading from SPI device " << mDevName << " a message of size " << msg.size();
-    throw std::system_error(std::error_code(errno, std::system_category()), os.str());
-  }   
+ bcm2835_spi_readnb(msg.data(), msg.size());
 }
 
 void SpiDev::write(const Message& msg){
- if(::write(mDev, msg.data(), msg.size())!=(ssize_t)msg.size()) {
-    std::ostringstream os;
-    os << "Error writing to SPI device " << mDevName << " a message of size " << msg.size();
-    throw std::system_error(errno, std::system_category(), os.str());
- }
+  bcm2835_spi_writenb(msg.data(), msg.size());
 }
 
 void SpiDev::transfer(Message& msg){
- spi_ioc_transfer trans;
- trans.tx_buf = (unsigned long)msg.data();
- trans.rx_buf = (unsigned long)msg.data();
- trans.len    = msg.size();
- if(::ioctl(mDev, SPI_IOC_MESSAGE(1), &trans)==-1){
-    std::ostringstream os;
-    os << "Error read/write with SPI device " << mDevName << " a transaction of size " << msg.size();
-    throw std::system_error(errno, std::system_category(), os.str());
- }
+ Message recv = msg;
+ bcm2835_spi_transfernb(msg.data(). recv.data(), msg.size());
+ msg = recv;
 }
 
 std::ostream& operator<<(std::ostream& o, const SpiDev::Message& msg) {
@@ -170,20 +133,15 @@ int main(int argc, char** argv) {
     return -1;
   }
   SpiDev spi(argv[1]);
-  SpiDev::Mode mode;
-  mode.clockPolarity = false;
-  mode.clockPhase    = false;
-  mode.lsbFirst      = false;
-  mode.loopback      = true;
-  spi.set(mode);
   spi.set(SpiDev::Speed(500000));
   spi.set(SpiDev::BitsPerWord(8));
   spi.set(SpiDev::BitAlignment(0));
   SpiDev::Message msg;
   auto i=std::back_inserter(msg);
+  *i++=0x81;
   while(*argv[2])
     *i++=*argv[2]++;
-  std::cout << "Transfer " << msg << " to SPI device " << spi.device() << std::endl;
+  std::cout << "Transfer " << msg << "(" << msg.size() << ") to SPI device " << spi.device() << std::endl;
   spi.transfer(msg);
   std::cout << "Answer was 0x" << std::hex << msg << std::endl;
   return 0;
